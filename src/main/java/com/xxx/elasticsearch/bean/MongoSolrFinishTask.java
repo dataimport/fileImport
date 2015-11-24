e0217ddd25495935471fda5b7535d637143f5297
liuhaibo <liuhaibo@news.cn>
2015-11-24 09:28:08 +0800

    错误处理

--- src/main/java/com/xxx/elasticsearch/bean/MongoSolrFinishTask.java
+++ src/main/java/com/xxx/elasticsearch/bean/MongoSolrFinishTask.java
@@ -29,14 +29,14 @@ public class MongoSolrFinishTask extends BaseTask{
 		super();
 	}
 	
-	public MongoSolrFinishTask(String uid, String tableName, String origin, String tags,
+	public MongoSolrFinishTask(String uid, String tableName, String tableNameAlias, String origin, String tags,
 			String[] columnName, Integer[] columnIndex, String separator,
 			String runTime, String startDate, String endDate, String filePath,
 			String fileName, long fileSize, String leftTime, long totalCount,
 			int taskStatus, String timeUse, int runNum, int updateOrAdd,
 			int beginLineNum, String createUser, boolean firstLineIgnore,
 			String id) {
-		super(uid, tableName, origin, tags, columnName, columnIndex, separator,
+		super(uid, tableName,tableNameAlias, origin, tags, columnName, columnIndex, separator,
 				runTime, startDate, endDate, filePath, fileName, fileSize,
 				leftTime, totalCount, taskStatus, timeUse, runNum, updateOrAdd,
 				beginLineNum, createUser, firstLineIgnore);
