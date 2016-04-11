adf5e28356a1b8222a6a3e9ec2b72bc6211f1caa
liuhaibo <liuhaibo1@jd.com>
2016-04-11 22:39:36 +0800

    查看服务器上的文件添加分页功能 查看文件导入状态页面的样式美化

--- src/main/java/com/xxx/admin/action/FolderAct.java
+++ src/main/java/com/xxx/admin/action/FolderAct.java
@@ -21,6 +21,7 @@ import com.xxx.admin.bean.Task;
 import com.xxx.admin.service.FolderService;
 import com.xxx.admin.service.TaskService;
 import com.xxx.core.exception.MongoDBException;
+import com.xxx.utils.Pagination;
 import com.xxx.utils.ResponseUtils;
 
 
@@ -29,6 +30,7 @@ import com.xxx.utils.ResponseUtils;
 @RequestMapping("/folder")
 public class FolderAct {
 	private static final Logger log = LoggerFactory.getLogger(FolderAct.class);
+
 
 	@RequestMapping(value = "add.htm")
 	public String addPath(Folder folder,ModelMap model,HttpServletRequest request,HttpServletResponse response) throws MongoDBException{		
@@ -63,13 +65,19 @@ public class FolderAct {
 	}
 	
 	@RequestMapping(value = "child.htm")
-	public String child(String folderPath,Boolean append,ModelMap model,HttpServletRequest request,HttpServletResponse response)throws MongoDBException {
+	public String child(String folderPath,String folderId,Boolean append,Integer pageNo, Integer pageSize,ModelMap model,HttpServletRequest request,HttpServletResponse response)throws MongoDBException {
 		long start = System.currentTimeMillis();	
 		List<Folder> folderList = folderService.list();	
 		log.debug(" 从mongodb中加载文件列表耗时："+(System.currentTimeMillis()-start)+" 毫秒");
 		String defaultPath = "";
 		if(StringUtils.isNotBlank(folderPath)){
 			defaultPath	= folderPath;
+		}else if(StringUtils.isNotBlank(folderId)){
+			List<Folder> fl =  folderService.findByFields(new String[]{"folderId"}, new String[]{folderId});
+			if(fl!=null&&fl.size()>0){
+				defaultPath = fl.get(0).getFolderPath();
+			}
+			model.put("folderId",folderId);
 		}else if(folderList.size()>0){
 			defaultPath = folderList.get(0).getFolderPath();			
 		}		
@@ -78,29 +86,59 @@ public class FolderAct {
 		if(StringUtils.isNotBlank(defaultPath)){
 			List fileTaskInfo =  new ArrayList<File>();
 			long start2 = System.currentTimeMillis();	
-			File[] childList = folderService.getChild(defaultPath);
+			File[] childList =  folderService.getChild(defaultPath);
 			log.debug(" 根据文件路径加载文件列表耗时："+(System.currentTimeMillis()-start2)+" 毫秒");
 			long start3 = System.currentTimeMillis();	
-			if(childList!=null){
-				int i=0;
-				for(File file : childList){
-					if(i==20){
-						break;
-					}
-					Task t = taskService.getTaskByFilePath(file.getPath());				
+			System.out.println("当前目录下的文件总数："+  childList.length);
+			if(pageNo==null){
+				pageNo=1;
+			}
+			if(pageSize==null){
+				pageSize=20;
+			}				
+			File[] files = new File[pageSize];
+			Pagination page = new Pagination(pageNo, pageSize, 0);
+			if(childList!=null){				
+				int fileSize = childList.length;
+				int begin = (pageNo-1)*pageSize>fileSize?fileSize:(pageNo-1)*pageSize;
+				int end = (begin+pageSize)>fileSize?fileSize:(begin+pageSize);
+				int index=0;		
+				if(end-begin<pageSize){
+					files = new File[end-begin];
+				}
+				for(int i=begin;i<end;i++){
+					files[index++] = childList[i];
+					Task t = taskService.getTaskByFilePath(childList[i].getPath());				
 					if(t==null){
 						t = new Task();
 					}
 					fileTaskInfo.add(t);	
-					i++;
-				}
-			}			
+				}					
+				page = new Pagination(pageNo, pageSize,fileSize);				
+//				int i=0;
+//				for(File file : childList){
+//					if(i==20){
+//						break;
+//					}
+//					Task t = taskService.getTaskByFilePath(file.getPath());				
+//					if(t==null){
+//						t = new Task();
+//					}
+//					fileTaskInfo.add(t);	
+//					i++;
+//				}
+			}		
+			
+			//request.getSession().setAttribute("childList", childList);//分布式部署后，需要改成缓存
 			log.debug(" 从mongodb中获取每个文件的任务信息耗时："+(System.currentTimeMillis()-start3)+" 毫秒");
 			model.put("fileTaskList", fileTaskInfo);
-			model.put("folderChild", childList);
+			//model.put("folderChild", childList);
+			model.put("folderChild", files);
+			model.put("page", page);
 		}else{
 			model.put("fileTaskList",new ArrayList<File>());
-			model.put("folderChild", new ArrayList<File>());			
+			model.put("folderChild", new ArrayList<File>());	
+			model.put("files", new File[1]);	
 		}
 				
 		if(append!=null&&append){
